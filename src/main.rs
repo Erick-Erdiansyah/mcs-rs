eeda8d64fc7ab289a9f76f68dd172c2eb64a8dd6
spirit <erdiansyah290@gmail.com>
2026-09-04 20:57:51 +0700

    no pong -_-

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-use bevy::prelude::*;
-
-use crate::pong::{ball::BallPlugin, paddle::PaddlePlugin};
-
-pub mod pong;
-
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, camera)
-        .add_plugins(PaddlePlugin)
-        .run();
-}
-
-pub fn camera(mut commands: Commands) {
-    commands.spawn(Camera2d);
+    println!("hello world");
 }
--- src/pong/ball/ball_components.rs
+++
@@ -1,6 +0,0 @@
-use bevy::prelude::*;
-
-#[derive(Component, Debug)]
-pub struct Ball {
-    pub velocity: Vec2,
-}
